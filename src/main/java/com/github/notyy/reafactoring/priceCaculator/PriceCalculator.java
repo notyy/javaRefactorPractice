c18d1a4a862c7fc52fed846786b83598226d8f50
诺铁 <notyycn@gmail.com>
2013-09-05 00:34:14 +0800

    introduce explaining variable

--- src/main/java/com/github/notyy/reafactoring/priceCaculator/PriceCalculator.java
+++ src/main/java/com/github/notyy/reafactoring/priceCaculator/PriceCalculator.java
@@ -11,4 +11,10 @@ public class PriceCalculator {
         }
         return basePrice * discountFactor;
     }
+
+    public double complexPrice(int quantity, double itemPrice) {
+        //price = base price - quantity discount + shipping
+        return quantity * itemPrice - Math.max(0, quantity-500) * itemPrice * 0.05 +
+                Math.min(quantity * itemPrice * 0.1, 100.0);
+    }
 }
---
+++ src/main/java/com/github/notyy/reafactoring/priceCaculator/readme.txt
@@ -0,0 +1,3 @@
+use this example to show
+1. replace temp with query (p120)
+2. introduce explaining variable
