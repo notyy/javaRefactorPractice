fcc4c83d8eea1b9b16529e7eea305a9575be1cc8
诺铁 <notyycn@gmail.com>
2013-09-04 22:59:20 +0800

    add simple refactorable owing print sample

---
+++ src/main/java/com/github/notyy/reafactoring/extractMethod/OrderItem.java
@@ -0,0 +1,17 @@
+package com.github.notyy.reafactoring.extractMethod;
+
+public class OrderItem {
+    private double amount = 0.0;
+
+    public OrderItem(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/com/github/notyy/reafactoring/extractMethod/OwingCaculator.java
@@ -0,0 +1,41 @@
+package com.github.notyy.reafactoring.extractMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OwingCaculator {
+
+    private String name;
+    private List<OrderItem> orderItems;
+
+    public OwingCaculator(String name,List<OrderItem> orderItems){
+        this.name = name;
+        this.orderItems = orderItems;
+    }
+
+    public void printOwing() {
+        double total = 0.0;
+        //print logo
+        System.out.println("********************");
+        System.out.println("***customer owing***");
+        System.out.println("********************");
+
+        //calculate total
+        for(OrderItem orderItem: orderItems){
+            total += orderItem.getAmount();
+        }
+
+        //print details
+        System.out.println("name:" + name);
+        System.out.println("amount:" + total);
+    }
+
+    public static void main(String[] args){
+        List<OrderItem> orderItems = new ArrayList<OrderItem>();
+        orderItems.add(new OrderItem(25.0));
+        orderItems.add(new OrderItem(35.0));
+        orderItems.add(new OrderItem(40.0));
+
+        new OwingCaculator("notyy", orderItems).printOwing();
+    }
+}
