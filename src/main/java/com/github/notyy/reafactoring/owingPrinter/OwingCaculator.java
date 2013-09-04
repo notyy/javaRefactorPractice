85563bb7850a23e37543e658f85f3a0647f6307e
诺铁 <notyycn@gmail.com>
2013-09-04 23:24:04 +0800

    good, ready for practice

--- src/main/java/com/github/notyy/reafactoring/owingPrinter/OwingCaculator.java
+++ src/main/java/com/github/notyy/reafactoring/owingPrinter/OwingCaculator.java
@@ -3,31 +3,36 @@ package com.github.notyy.reafactoring.owingPrinter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SimpleOwingCaculator {
+public class OwingCaculator {
 
     private String name;
     private List<OrderItem> orderItems;
 
-    public SimpleOwingCaculator(String name, List<OrderItem> orderItems){
+    public OwingCaculator(String name, List<OrderItem> orderItems){
         this.name = name;
         this.orderItems = orderItems;
     }
 
     public void printOwing() {
-        double total = 0.0;
         //print logo
         System.out.println("********************");
         System.out.println("***customer owing***");
         System.out.println("********************");
 
         //calculate total
-        for(OrderItem orderItem: orderItems){
-            total += orderItem.getAmount();
-        }
+        double total = calculateTotal(orderItems);
 
         //print details
         System.out.println("name:" + name);
         System.out.println("amount:" + total);
+    }
+
+    private double calculateTotal(List<OrderItem> orderItems1) {
+        double total = 0.0;
+        for(OrderItem orderItem: orderItems1){
+            total += orderItem.getAmount();
+        }
+        return total;
     }
 
     public static void main(String[] args){
@@ -36,6 +41,6 @@ public class SimpleOwingCaculator {
         orderItems.add(new OrderItem(35.0));
         orderItems.add(new OrderItem(40.0));
 
-        new SimpleOwingCaculator("notyy", orderItems).printOwing();
+        new OwingCaculator("notyy", orderItems).printOwing();
     }
 }
