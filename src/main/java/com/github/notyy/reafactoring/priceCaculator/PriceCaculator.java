e024cb02202e88e6a0fc569b15c24a599b4c2415
诺铁 <notyycn@gmail.com>
2013-09-04 23:58:07 +0800

    add example to show replace temp with query

---
+++ src/main/java/com/github/notyy/reafactoring/priceCaculator/PriceCaculator.java
@@ -0,0 +1,14 @@
+package com.github.notyy.reafactoring.priceCaculator;
+
+public class PriceCaculator {
+    public double getPrice(int quantity, double itemPrice) {
+        double basePrice = quantity * itemPrice;
+        double discountFactor;
+        if(basePrice > 1000){
+            discountFactor = 0.80;
+        }else {
+            discountFactor = 0.90;
+        }
+        return basePrice * discountFactor;
+    }
+}
