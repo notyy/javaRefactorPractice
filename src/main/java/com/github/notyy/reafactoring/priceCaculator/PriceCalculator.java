9aeb492aa42150d62790e7a0f4e2a4fc0b0a6b6c
notyy <notyycn@gmail.com>
2013-12-16 02:44:02 +0800

    upgrade price calculator

--- src/main/java/com/github/notyy/reafactoring/priceCaculator/PriceCalculator.java
+++ src/main/java/com/github/notyy/reafactoring/priceCaculator/PriceCalculator.java
@@ -1,7 +1,15 @@
 package com.github.notyy.reafactoring.priceCaculator;
 
 public class PriceCalculator {
-    public double getPrice(int quantity, double itemPrice) {
+    private final int quantity;
+    private final double itemPrice;
+
+    public PriceCalculator(int quantity, double itemPrice){
+        this.quantity = quantity;
+        this.itemPrice = itemPrice;
+    }
+
+    public double getPrice() {
         double basePrice = quantity * itemPrice;
         double discountFactor;
         if(basePrice >= 1000){
