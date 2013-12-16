8b38edd6b10ef38d2df265e62fc8dfc9faea24b3
notyy <notyycn@gmail.com>
2013-12-16 09:02:52 +0800

    add a new isFlaged function

--- src/main/java/com/github/notyy/reafactoring/gameBoard/MineSweeper.java
+++ src/main/java/com/github/notyy/reafactoring/gameBoard/MineSweeper.java
@@ -9,10 +9,20 @@ public class MineSweeper {
     public List<int[]> getThem() {
         List<int[]> list1 = new ArrayList<int[]>();
         for(int[] x: theList){
-            if(x[0] == 4){
+            if(x[0] == 1){
                 list1.add(x);
             }
         }
         return list1;
+    }
+
+    public boolean isFlaged(int x, int y) {
+        List<int[]> list2 = getThem();
+        for(int[] i: list2){
+            if(i[1] == x && i[2] == y){
+                return true;
+            }
+        }
+        return false;
     }
 }
