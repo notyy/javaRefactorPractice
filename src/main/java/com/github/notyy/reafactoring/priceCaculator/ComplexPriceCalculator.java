5ff0d3ba0ad0165f9f68f632c5715d81007fb473
notyy <notyycn@gmail.com>
2014-05-12 15:35:14 +0800

    add comment to make things clear

--- src/main/java/com/github/notyy/reafactoring/priceCaculator/ComplexPriceCalculator.java
+++ src/main/java/com/github/notyy/reafactoring/priceCaculator/ComplexPriceCalculator.java
@@ -5,7 +5,7 @@ public class ComplexPriceCalculator {
     }
 
     public double complexPrice(int quantity, double itemPrice) {
-        //price = base price - quantity discount + shipping
+        //price = base price - quantity discount + shipping fee
         return quantity * itemPrice - Math.max(0, quantity - 500) * itemPrice * 0.05 +
                 Math.min(quantity * itemPrice * 0.1, 100.0);
     }
